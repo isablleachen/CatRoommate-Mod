e5809d6a3f07350e0e6bd5957847369fbca7daad
Isabella Chen <xc1338@nyu.edu>
2019-03-14 12:09:37 -0400

    Add basic assets for the open scene & selection scene

---
+++ CatRoommate Mod (assignment4)/Assets/Scripts/OpenSceneButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenSceneButton : MonoBehaviour
+{
+    public void SelectionScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); 
+    }
+
+    public void InstructionScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
